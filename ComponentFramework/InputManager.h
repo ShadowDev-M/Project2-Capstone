c5a11498e626f2624f37985a533cc7908a569fd7
Akamundi <145157267+Akamundi@users.noreply.github.com>
2025-08-25 20:08:48 -0400

    InputManager: HandleEvents update

--- ComponentFramework/InputManager.h
+++ ComponentFramework/InputManager.h
@@ -10,6 +10,12 @@
 #include "TransformComponent.h"
 #include <memory>
 #include "CameraActor.h"
+#include "imgui.h"
+#include "CollisionSystem.h"
+
+
+using namespace ImGui;
+
 
 enum class InputState {
 	Released,
@@ -17,46 +23,197 @@ enum class InputState {
 	Held // For continuous input checks
 };
 
-class keyboardInputMap {
-private:
+
+class InputMap {
+protected:
 
 	//mapping a keyCode with it's current state
 	std::map<int, InputState> keyStates;
 
 public:
-	bool isActive(int keyCode) {
+	virtual bool isActive(int keyCode) {
 		//Active == both pressed and held
 		if (keyStates[keyCode] == InputState::Pressed) return true;
 
 		return keyStates[keyCode] == InputState::Held;
 	}
-	
-	bool isPressed(int keyCode) {
+
+	virtual bool isPressed(int keyCode) {
 		return keyStates[keyCode] == InputState::Pressed;
 	}
 
-	bool isReleased(int keyCode) {
+	virtual bool isReleased(int keyCode) {
 		return keyStates[keyCode] == InputState::Released;
 	}
 
-	bool isHeld(int keyCode) {
+	virtual bool isHeld(int keyCode) {
 		return keyStates[keyCode] == InputState::Held;
 	}
 
-	void toggleKeyPress(int keyCode) {
+	virtual void toggleKeyPress(int keyCode) {
 		keyStates[keyCode] = InputState::Pressed;
 	}
 
-	void toggleKeyRelease(int keyCode) {
+	virtual void toggleKeyRelease(int keyCode) {
 		keyStates[keyCode] = InputState::Released;
 	}
 
-	void toggleKeyHeld(int keyCode) {
+	virtual void toggleKeyHeld(int keyCode) {
 		keyStates[keyCode] = InputState::Held;
 	}
 
-	void update(float deltaTime) {
+	virtual void update(const float deltaTime_) = 0;
+};
+
+class mouseInputMap : public InputMap {
+private:
+
+
+public:
+
+	void HandleEvents(const SDL_Event& sdlEvent, SceneGraph* sceneGraph, CollisionSystem* collisionSystem) {
+		if (GetIO().WantCaptureMouse) {
+			return;
+		}
+		static int lastX = 0, lastY = 0;
+		const Uint8* keys = SDL_GetKeyboardState(NULL);
+
+
+
+		static bool mouseHeld = false;
+
+		switch (sdlEvent.type) {
+		case SDL_MOUSEBUTTONDOWN:
+			toggleKeyPress(sdlEvent.button.button);
+			std::cout << static_cast<int>(sdlEvent.button.button) << " is Pressed!" << std::endl;
+
+			lastX = sdlEvent.button.x;
+			lastY = sdlEvent.button.y;
+
+			if (sdlEvent.button.button == SDL_BUTTON_RIGHT) {
+				mouseHeld = false;
+			}
+			if (sdlEvent.button.button == SDL_BUTTON_LEFT) {
+
+				// makes it so ImGui handles the mouse click
+				if (GetIO().WantCaptureMouse) {
+					mouseHeld = false;
+					return;
+				}
+
+				lastX = sdlEvent.button.x;
+				lastY = sdlEvent.button.y;
+
+				Ref<CameraActor> camera = std::dynamic_pointer_cast<CameraActor>(sceneGraph->GetActor("camera"));
+
+				Vec3 startPos = camera->GetComponent<TransformComponent>()->GetPosition();
+				Vec3 endPos = startPos + Raycast::screenRayCast(lastX, lastY, camera->GetProjectionMatrix(), camera->GetViewMatrix());
+
+
+				//prepare for unintelligible logic for selecting 
+				Ref<Actor> raycastedActor = collisionSystem->PhysicsRaycast(startPos, endPos);
+
+				//(Slightly) more expensive debug selector if nothing was selected with the ColliderComponent PhysicsRaycast
+				if (!raycastedActor) raycastedActor = sceneGraph->MeshRaycast(startPos, endPos);
+
+				//an object was clicked
+				if (raycastedActor) {
+
+
+					if (!keys[SDL_SCANCODE_LCTRL] && !(sceneGraph->debugSelectedAssets.find(raycastedActor->getActorName()) != sceneGraph->debugSelectedAssets.end())) { sceneGraph->debugSelectedAssets.clear(); }
+
+					if (sceneGraph->debugSelectedAssets.find(raycastedActor->getActorName()) != sceneGraph->debugSelectedAssets.end() && keys[SDL_SCANCODE_LCTRL]) { sceneGraph->debugSelectedAssets.erase(raycastedActor->getActorName()); }
+
+					else sceneGraph->debugSelectedAssets.emplace(raycastedActor->getActorName(), raycastedActor);
+
+				}
+				//no object was clicked, and left control isn't pressed (making sure the user didn't accidentally misclicked during multi object selection before clearing selection)
+				else if (!keys[SDL_SCANCODE_LCTRL])sceneGraph->debugSelectedAssets.clear();
+
+
+
+				//startPos.print();
+				//endPos.print();
+				mouseHeld = false;
+
+			}
+
+
+
+			break;
+		case SDL_MOUSEBUTTONUP:
+			toggleKeyRelease(sdlEvent.button.button);
+			std::cout << static_cast<int>(sdlEvent.button.button) << " is Released!" << std::endl;
+
+			break;
+		case SDL_MOUSEMOTION:
+			if (isHeld(sdlEvent.button.button) || isPressed(sdlEvent.button.button)) {
+
+				// makes it so ImGui handles the mouse motion
+				if (GetIO().WantCaptureMouse) {
+					toggleKeyPress(sdlEvent.button.button);
+					return;
+				}
+
+				if (!(sceneGraph->debugSelectedAssets.empty())) {
+					//get direction vector of new vector of movement from old mouse pos and new mouse pos
+
+					int deltaX = sdlEvent.motion.x - lastX;
+					int deltaY = sdlEvent.motion.y - lastY;
+					lastX = sdlEvent.motion.x;
+					lastY = sdlEvent.motion.y;
+
+					auto& debugGraph = sceneGraph->debugSelectedAssets;
+					Ref<CameraActor> camera = std::dynamic_pointer_cast<CameraActor>(sceneGraph->GetActor("camera"));
+
+
+					for (const auto& obj : debugGraph) {
+						Ref<TransformComponent> transform = obj.second->GetComponent<TransformComponent>();
+						Vec3 vectorMove = transform->GetPosition() + Vec3(deltaX, -deltaY, transform->GetPosition().z) * (camera->GetComponent<TransformComponent>()->GetPosition().z - transform->GetPosition().z) / 40 * 0.045;
+						transform->SetPos(vectorMove.x, vectorMove.y, transform->GetPosition().z);
+
+					}
+				}
+
+
+
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	void update(const float deltaTime) override {
 		
+		
+		
+		for (auto& keyCode : keyStates) {
+
+			//If pressed, do behavior and then promote it to being held
+			if (keyCode.second == InputState::Pressed) {
+				//TODO: Pressed behavior
+
+
+
+
+				std::cout << keyCode.first << " Promoted to HELD" << std::endl;
+				toggleKeyHeld(keyCode.first);
+			}
+
+		}
+	}
+};
+
+
+class keyboardInputMap : public InputMap {
+private:
+
+	
+public:
+
+	void update(const float deltaTime) override {
 		
 		
 		
@@ -100,9 +257,6 @@ public:
 			
 		}
 		
-		
-
-
 	}
 
 };
@@ -119,6 +273,7 @@ private:
 	InputManager& operator = (InputManager&&) = delete;
 
 	keyboardInputMap keyboard;
+	mouseInputMap mouse;
 
 
 public:
@@ -135,7 +290,7 @@ public:
 
 		//update keyboard object
 		keyboard.update(deltaTime);
-
+		mouse.update(deltaTime);
 		
 		//Check which scene debug vs playing
 		if (true) { //temp set to always true until we can define debug vs playable scenes
@@ -158,7 +313,7 @@ public:
 		Ref<CameraActor> camera = std::dynamic_pointer_cast<CameraActor>(sceneGraph->GetActor("camera"));
 		if (camera) {
 			for (auto& keyPress : inputMap) {
-
+				
 				if (keyboard.isPressed(keyPress.first)) {
 
 					//Put a slider here for stud based movement
@@ -202,6 +357,14 @@ public:
 
 		}
 	}
+
+	void HandleEvents(const SDL_Event& sdlEvent, SceneGraph* sceneGraph, CollisionSystem* collisionSystem) {
+
+		mouse.HandleEvents(sdlEvent, sceneGraph, collisionSystem);
+
+		
+	}
+
 
 	~InputManager() { }
 };
